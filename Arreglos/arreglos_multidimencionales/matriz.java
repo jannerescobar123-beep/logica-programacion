733141357eb8e24bb8665c2d600d1d883bd018be
janner escobar <jannerescobar123@gmail.com>
2025-11-21 14:38:01 -0500

    feat: se termina trabajo 1 de arreglos multidimencionales

--- Arreglos/arreglos_multidimencionales/matriz.java
+++ Arreglos/arreglos_multidimencionales/matriz.java
@@ -1,5 +1,21 @@
 package Arreglos.arreglos_multidimencionales;
 
 public class matriz {
-    
+    public static void main(String[] args) {
+        char[][] Matriz = new char[5][5];
+        
+        for (int i = 0; i < Matriz.length; i++) {
+            for (int j = 0; j < Matriz[i].length; j++) {
+                Matriz[i][j] = '#';
+            }
+        }
+
+        System.out.println("\nla matriz es:");
+        for (int i = 0; i < Matriz.length; i++) {
+            for (int j = 0; j < Matriz[i].length; j++) {
+                System.out.print(Matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
